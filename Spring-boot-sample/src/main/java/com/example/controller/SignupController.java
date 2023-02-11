7831a13d3fa574a013c7b12f705300fb3c5dbb60
yukishi24 <yukishi24github@gmail.com>
2023-02-11 22:59:39 +0900

    add: Validation

--- Spring-boot-sample/src/main/java/com/example/controller/SignupController.java
+++ Spring-boot-sample/src/main/java/com/example/controller/SignupController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,7 +54,7 @@ public class SignupController {
    * @return
    */
   @PostMapping("/signup")
-  public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form,
+  public String postSignup(Model model, Locale locale, @ModelAttribute @Validated SignupForm form,
       BindingResult bindingResult) {
 
     // 入力チェック
