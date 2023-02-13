ea7653311af060f47742b6fc709eb9e241592cb0
yukishi24 <yukishi24github@gmail.com>
2023-02-13 23:03:32 +0900

    add: controller

--- Spring-boot-sample/src/main/java/com/example/controller/LoginController.java
+++ Spring-boot-sample/src/main/java/com/example/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Login画面への遷移コントローラー
@@ -20,5 +21,15 @@ public class LoginController {
   @GetMapping("/login")
   public String getLogin() {
     return "login/login";
+  }
+
+  /**
+   * ユーザー一覧画面にリダイレクト
+   * 
+   * @return
+   */
+  @PostMapping("/login")
+  public String postLogin() {
+    return "redirect:/user/list";
   }
 }
