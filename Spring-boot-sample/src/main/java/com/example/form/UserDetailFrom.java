58e16ee3458fe3e99d51e392e69bfd916d5fe3d7
yukishi24 <yukishi24github@gmail.com>
2023-03-07 22:11:13 +0900

    add: detail.html

---
+++ Spring-boot-sample/src/main/java/com/example/form/UserDetailFrom.java
@@ -0,0 +1,14 @@
+package com.example.form;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public final class UserDetailFrom {
+  private String userId;
+  private String password;
+  private String userName;
+  private Date birthday;
+  private Integer age;
+  private Integer gender;
+}
