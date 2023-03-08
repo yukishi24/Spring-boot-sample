974a0481aa785e002f9070f610355d1ee95f1098
yukishi24 <yukishi24github@gmail.com>
2023-03-08 22:37:25 +0900

    modify: UserMapper.java

--- Spring-boot-sample/src/main/java/com/example/repository/UserMapper.java
+++ Spring-boot-sample/src/main/java/com/example/repository/UserMapper.java
@@ -2,6 +2,7 @@ package com.example.repository;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import com.example.domain.user.model.MUser;
 
 @Mapper
@@ -14,4 +15,11 @@ public interface UserMapper {
 
   // ユーザー取得(一件取得)
   public MUser findOne(String userId);
+
+  // ユーザー更新
+  public void updateOne(@Param("userId") String userid, @Param("password") String password,
+      @Param("userName") String userName);
+
+  // ユーザー削除
+  public int deleteOne(@Param("userId") String userId);
 }
