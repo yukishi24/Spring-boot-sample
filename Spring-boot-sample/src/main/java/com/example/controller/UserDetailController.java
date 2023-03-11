98e132cd50c5bf4ea9d54af26e86566574668569
yukishi24 <yukishi24github@gmail.com>
2023-03-12 00:04:37 +0900

    modify: UserDetailController.java

--- Spring-boot-sample/src/main/java/com/example/controller/UserDetailController.java
+++ Spring-boot-sample/src/main/java/com/example/controller/UserDetailController.java
@@ -15,51 +15,30 @@ import com.example.form.UserDetailForm;
 @Controller
 @RequestMapping("/user")
 public class UserDetailController {
-
   @Autowired
   private UserService userService;
+
   @Autowired
   private ModelMapper modelMapper;
 
-  // ユーザー詳細を表示
   @GetMapping("/detail/{userId:.+}")
-  public String getUser(UserDetailForm form, Model model,
-      @PathVariable("userId") String userId) {
-    // ユーザー一件取得
+  public String getUser(UserDetailForm form, Model model, @PathVariable("userId") String userId) {
     MUser user = userService.getUserOne(userId);
     user.setPassword(null);
-    form.setUserName(user.getUserName());
-    form.setBirthday(user.getBirthday());
-    form.setAge(user.getAge());
-    // MUserをformに変換
     form = modelMapper.map(user, UserDetailForm.class);
-
-    // modelに登録
-    model.addAttribute("detailFrom", form);
-
-    // ユーザー詳細画面の表示
+    model.addAttribute("userDetailForm", form);
     return "user/detail";
   }
 
-  // ユーザー更新処理
   @PostMapping(value = "/detail", params = "update")
   public String updateUser(UserDetailForm form, Model model) {
-
-    // ユーザー更新
     userService.updateOne(form.getUserId(), form.getPassword(), form.getUserName());
-
-    // ユーザー一覧のリダイレクト
     return "redirect:/user/list";
   }
 
-  // ユーザー削除(一件)
-  @PostMapping(value = "/detai", params = "delete")
+  @PostMapping(value = "/detail", params = "delete")
   public String deleteUser(UserDetailForm form, Model model) {
-
-    // ユーザー削除
     userService.deleteOne(form.getUserId());
-
-    // ユーザー一覧画面にリダイレクト
     return "redirect:/user/list";
   }
 }
