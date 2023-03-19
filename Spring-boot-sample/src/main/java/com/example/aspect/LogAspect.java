44d040c7d84fdb75cd8c9794dd1a7483f962990a
yukishi24 <yukishi24github@gmail.com>
2023-03-19 22:51:46 +0900

    modify: LogAspect.java

--- Spring-boot-sample/src/main/java/com/example/aspect/LogAspect.java
+++ Spring-boot-sample/src/main/java/com/example/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.example.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -29,5 +31,37 @@ public class LogAspect {
   @After("execution(* *..*.*UserService.*(..)))")
   public void endLog(JoinPoint jp) {
     log.info("メソッド終了: " + jp.getSignature());
+  }
+
+  /**
+   * コントローラーの実行前後にログを出力。
+   * 
+   * @param jp
+   * @return
+   * @throws Throwable
+   */
+  @Around("@within(org.springframework.stereotype.Controller)")
+  public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+
+    // 開始ログ
+    log.info("メソッド開始: " + jp.getSignature());
+
+    try {
+      // メソッド実行
+      Object result = jp.proceed();
+
+      // 終了ログ出力
+      log.info("メソッド終了: " + jp.getSignature());
+
+      // 実行結果を呼び出し元に返す
+      return result;
+
+    } catch (Exception e) {
+      // エラーログを出力
+      log.error("メソッド異常終了: " + jp.getSignature());
+
+      // エラーの再スロー
+      throw e;
+    }
   }
 }
