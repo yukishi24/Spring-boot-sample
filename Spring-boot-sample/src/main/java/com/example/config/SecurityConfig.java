dbb2bf35ea9646add6b7810a1126230403fa4f51
yukishi24 <yukishi24github@gmail.com>
2023-03-25 21:52:16 +0900

    modify: SecurityConfig.java

--- Spring-boot-sample/src/main/java/com/example/config/SecurityConfig.java
+++ Spring-boot-sample/src/main/java/com/example/config/SecurityConfig.java
@@ -1,22 +1,25 @@
 package com.example.config;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
+
+  @Autowired
+  private UserDetailsService userDetailsService;
 
   /**
    * セキュリティの対象外の設定
@@ -43,32 +46,44 @@ public class SecurityConfig {
         .failureUrl("/login?error") // 失敗時
         .usernameParameter("userId").passwordParameter("password")
         .defaultSuccessUrl("/user/list", true))
-        .authorizeHttpRequests(authz -> authz
-            .requestMatchers("/login").permitAll() // 直リンクOK
+        .authorizeHttpRequests(authz -> authz.requestMatchers("/login").permitAll() // 直リンクOK
             .requestMatchers("/user/signup").permitAll() // 直リンクOK
             .anyRequest().authenticated());
     http.csrf().disable();
     return http.build();
   }
 
-  /**
-   * 認証の設定
-   * 
-   * @return
-   */
   @Bean
-  public InMemoryUserDetailsManager userDetailsService() {
-    // インメモリ認証
-    PasswordEncoder encoder = passwordEncoder();
-    UserDetails user = // userを追加
-        User.withUsername("user").password(encoder.encode("user")).roles("GENERAL").build();
-    UserDetails admin = // adimnを追加
-        User.withUsername("admin").password(encoder.encode("admin")).roles("ADMIN").build();
-    return new InMemoryUserDetailsManager(user, admin);
+  public UserDetailsManager users(DataSource dataSource) {
+    String userQuery =
+        "select user_id as username,password,true as enabled from m_user where user_id = ?";
+    String authoritiesQuery = "select user_id,role from m_user where user_id = ?";
+
+    JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    users.setUsersByUsernameQuery(authoritiesQuery);
+    users.setAuthoritiesByUsernameQuery(authoritiesQuery);
+
+    return users;
   }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+  // /**
+  // * 認証の設定
+  // *
+  // * @return
+  // */
+  // @Bean
+  // public InMemoryUserDetailsManager userDetailsService() {
+  // // インメモリ認証
+  // PasswordEncoder encoder = passwordEncoder();
+  // UserDetails user = // userを追加
+  // User.withUsername("user").password(encoder.encode("user")).roles("GENERAL").build();
+  // UserDetails admin = // adimnを追加
+  // User.withUsername("admin").password(encoder.encode("admin")).roles("ADMIN").build();
+  // return new InMemoryUserDetailsManager(user, admin);
+  // }
+  //
+  // @Bean
+  // public PasswordEncoder passwordEncoder() {
+  // return new BCryptPasswordEncoder();
+  // }
 }
