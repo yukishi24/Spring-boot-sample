4b8b7ceb668e73d238dc6041633d41668e7dcb19
yukishi24 <yukishi24github@gmail.com>
2023-03-26 21:40:28 +0900

    modify: SecurityConfig.java

--- Spring-boot-sample/src/main/java/com/example/config/SecurityConfig.java
+++ Spring-boot-sample/src/main/java/com/example/config/SecurityConfig.java
@@ -1,25 +1,25 @@
 package com.example.config;
 
 import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
-  @Autowired
-  private UserDetailsService userDetailsService;
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   /**
    * セキュリティの対象外の設定
@@ -60,7 +60,7 @@ public class SecurityConfig {
     String authoritiesQuery = "select user_id,role from m_user where user_id = ?";
 
     JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-    users.setUsersByUsernameQuery(authoritiesQuery);
+    users.setUsersByUsernameQuery(userQuery);
     users.setAuthoritiesByUsernameQuery(authoritiesQuery);
 
     return users;
@@ -81,9 +81,5 @@ public class SecurityConfig {
   // User.withUsername("admin").password(encoder.encode("admin")).roles("ADMIN").build();
   // return new InMemoryUserDetailsManager(user, admin);
   // }
-  //
-  // @Bean
-  // public PasswordEncoder passwordEncoder() {
-  // return new BCryptPasswordEncoder();
-  // }
+
 }
