e2e18c014ea0360edc62caa04c860622335802e3
yukishi24 <yukishi24github@gmail.com>
2023-04-03 21:55:38 +0900

    modify: detail.html

--- Spring-boot-sample/src/main/java/com/example/rest/UserRestController.java
+++ Spring-boot-sample/src/main/java/com/example/rest/UserRestController.java
@@ -14,7 +14,7 @@ import com.example.form.UserDetailForm;
 public class UserRestController {
 
 	@Autowired
-	private UserService userService;
+	UserService userService;
 
 	/**ユーザー更新
 	 * @param form
