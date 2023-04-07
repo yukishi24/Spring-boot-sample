daea1ef5368255af2c2d8556ec28cf953d2bbe9c
yukishi24 <yukishi24github@gmail.com>
2023-04-08 00:10:13 +0900

    modify: MUser

--- Spring-boot-sample/src/main/java/com/example/domain/user/model/MUser.java
+++ Spring-boot-sample/src/main/java/com/example/domain/user/model/MUser.java
@@ -2,24 +2,32 @@ package com.example.domain.user.model;
 
 import java.util.Date;
 import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "muser")
 public class MUser {
+	@Id
+	private String userId;
+	private String password;
+	private String userName;
 
-  private String userId;
-  private String password;
-  private String userName;
+	private Date birthday;
 
-  private Date birthday;
+	private Integer age;
+	private Integer gender;
 
-  private Integer age;
-  private Integer gender;
-  private Integer departmentId;
+	private Integer departmentId;
 
-  private String role;
-
-  private Department department;
-
-  private List<Salary> salaryList;
+	private String role;
+	@Transient
+	private Department department;
+	@Transient
+	private List<Salary> salaryList;
 }
